4fe1e6506b46bd1233a02ca1f86a481271086f11
SnowCait <SnowCait@users.noreply.github.com>
2022-03-27 01:53:48 +0900

    WIP

---
+++ DddTwitter/Domain/Models/Users/ScreenName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DddTwitter.Domain.Models.Users
+{
+    class ScreenName
+    {
+    }
+}
---
+++ DddTwitter/Domain/Models/Users/UserId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DddTwitter.Domain.Models.Users
+{
+    class UserId : IEquatable<UserId>
+    {
+        public UserId(long value)
+        {
+            Value = value;
+        }
+
+        public long Value { get; }
+
+        public bool Equals([AllowNull] UserId other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
